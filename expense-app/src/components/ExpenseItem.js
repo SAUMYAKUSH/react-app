6ebae4a0b98afd81bbb18e468dde6c227cc7a051
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2023-12-16 00:35:55 +0530

    first react project

---
+++ expense-app/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h1>Expense Items</h1>
+      </div>
+      <div>
+        <h2>Food</h2>
+        <div>Rs 10</div>
+      </div>
+      <div>
+        <h2>Petrol</h2>
+        <div>Rs 100</div>
+      </div>
+      <div>
+        <h2>Moies</h2>
+         <div>Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
