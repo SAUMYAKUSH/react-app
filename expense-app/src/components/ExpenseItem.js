4a7218f8cf190074baf33ff687cec3d86883aac5
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2023-12-16 13:23:34 +0530

    ques8

--- expense-app/src/components/ExpenseItem.js
+++ expense-app/src/components/ExpenseItem.js
@@ -1,23 +1,30 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'food';
+  const expenseAmount = 10;
+  const LocationOfExpenditure = "Prayagraj";
   return (
-    <div>
+    <div className="expense-item">
       <div>
         <h1>Expense Items</h1>
       </div>
-      <div>
-        <h2>Food</h2>
-        <div>Rs 10</div>
+      <div style={{display:"flex","flex-direction":"column"}}>
+
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs{expenseAmount}</div>
+        <div className="expense-item__price">{LocationOfExpenditure}</div>
       </div>
-      <div>
+      <div className="expense-item__description">
         <h2>Petrol</h2>
-        <div>Rs 100</div>
+        <div className="expense-item__price">Rs{expenseAmount}</div>
       </div>
-      <div>
-        <h2>Moies</h2>
-         <div>Rs 200</div>
-      </div>
+      
+    </div>
     </div>
   );
 }
 
-export default ExpenseItem;
+export default ExpenseItem; 
