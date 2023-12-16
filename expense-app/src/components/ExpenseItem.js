390d208986719e1d2b9d040f4f912d3ede069fb5
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2023-12-17 00:22:32 +0530

    ques9

--- expense-app/src/components/ExpenseItem.js
+++ expense-app/src/components/ExpenseItem.js
@@ -1,28 +1,19 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'food';
-  const expenseAmount = 10;
-  const LocationOfExpenditure = "Prayagraj";
+function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>
-        <h1>Expense Items</h1>
-      </div>
-      <div style={{display:"flex","flex-direction":"column"}}>
+      <div>{props.date.toISOString()}</div>
+
 
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">Rs{expenseAmount}</div>
-        <div className="expense-item__price">{LocationOfExpenditure}</div>
-      </div>
-      <div className="expense-item__description">
-        <h2>Petrol</h2>
-        <div className="expense-item__price">Rs{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs{props.amount}</div>
+        <div className="expense-item__place">{props.LocationOfExpense}</div>
+        
       </div>
       
-    </div>
     </div>
   );
 }
