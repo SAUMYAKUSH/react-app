778250da971d663f989aa353757cdeba8bbf47ec
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2024-01-03 21:01:07 +0530

    ques12

---
+++ expense-app/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import Card from "../UI/card";
+import Expensedate from "./Expensedate";
+import "./ExpenseItem.css"
+
+const ExpenseItem = (props) => {
+
+  return (
+    <Card className="expense-item">
+      <Expensedate date={props.date}></Expensedate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs{props.amount}</div>
+        <div className="expense-item__place">{props.LocationOfExpense}</div>
+        
+      </div>
+      
+    </Card>
+  );
+}
+
+export default ExpenseItem; 
