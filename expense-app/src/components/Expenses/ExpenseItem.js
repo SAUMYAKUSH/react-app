ce010914c8d7c8bb635dd79794ce64f4111ddb27
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2024-01-10 13:19:51 +0530

    ques9

--- expense-app/src/components/Expenses/ExpenseItem.js
+++ expense-app/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/card";
 import Expensedate from "./Expensedate";
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
- const[title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluatd by React');
- const clickHandler =() =>{
-  setTitle('Updated!');
-  console.log(title);
- };
+ 
   return (
     <Card className="expense-item">
       <Expensedate date={props.date}></Expensedate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">Rs{props.amount}</div>
         <div className="expense-item__place">{props.LocationOfExpense}</div>
         
       </div>
-      <button onClick={clickHandler}>Change Title</button>
       
     </Card >
   );
