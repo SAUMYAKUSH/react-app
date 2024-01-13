843ad18ee89f36ffc49594c512c38d4bf3211d0c
SAUMYAKUSH <saumyakushwaha803@gmail.com>
2024-01-13 22:33:54 +0530

    ques14

--- expense-app/src/components/Expenses/Expenses.js
+++ expense-app/src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020"); //date select krne k bad waps ye compo chale isliye state
@@ -26,6 +26,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses}/>
        <ExpensesList  items={filteredExpenses}/>
     </Card>
     </div>
